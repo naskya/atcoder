d5cc4531358dfd3c55533af2a42bae5c9b0a1366
naskya <m@naskya.net>
2022-08-26 15:42:05 +0900

    Add header files

---
+++ header/headers.hpp
@@ -0,0 +1,36 @@
+#ifndef HEADERS_HPP
+#  define HEADERS_HPP
+#  include <cassert>
+#  include <cctype>
+#  include <cmath>
+#  include <cstddef>
+#  include <cstdint>
+#  include <cstdlib>
+#  include <algorithm>
+#  include <array>
+#  include <bitset>
+#  include <complex>
+#  include <deque>
+#  include <functional>
+#  include <iostream>
+#  include <iomanip>
+#  include <iterator>
+#  include <limits>
+#  include <list>
+#  include <map>
+#  include <numeric>
+#  include <queue>
+#  include <regex>
+#  include <set>
+#  include <sstream>
+#  include <stack>
+#  include <string>
+#  include <string_view>
+#  include <tuple>
+#  include <type_traits>
+#  include <tuple>
+#  include <utility>
+#  include <vector>
+#  include "debug_print.hpp"
+#endif
+
