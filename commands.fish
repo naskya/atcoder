b435778b0e205ec5e04fbf556e71d234d98a321e
naskya <m@naskya.net>
2022-08-30 04:48:20 +0900

    Add commands for generating random test cases

--- commands.fish
+++ commands.fish
@@ -20,66 +20,111 @@ function contest -d "Prepare for atcoder contests"
 end
 
 
-function cx
+function compile_debug_on_print_on
     clang++                                                                               \
                                                                                           \
     -std=c++17 -stdlib=libc++                                                             \
     -DLOCAL -DDEBUG -DDEBUG_PRINT                                                         \
     -D_LIBCPP_DEBUG_LEVEL=2 -O0 -g3 -fsanitize=address,undefined                          \
-    -include-pch {$atcoder_dir}/header/precompiled/debug_on_print_on/headers.hpp.pch \
+    -include-pch {$atcoder_dir}/header/precompiled/debug_on_print_on/headers.hpp.pch      \
                                                                                           \
     -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion     \
     -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo         \
-    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                  \
-                                                                                          \
-    -o a main.cpp
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1 $argv
 end
 
 
-function cxx
+function cx
+    compile_debug_on_print_on -o a main.cpp
+end
+
+
+function compile_debug_on_print_off
     clang++                                                                                \
                                                                                            \
     -std=c++17 -stdlib=libc++                                                              \
     -DLOCAL -DDEBUG                                                                        \
     -D_LIBCPP_DEBUG_LEVEL=2 -O0 -g3 -fsanitize=address,undefined                           \
-    -include-pch {$atcoder_dir}/header/precompiled/debug_on_print_off/headers.hpp.pch \                                                                                
+    -include-pch {$atcoder_dir}/header/precompiled/debug_on_print_off/headers.hpp.pch      \
                                                                                            \
     -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion      \
     -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo          \
-    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                   \
-                                                                                           \
-    -o a main.cpp
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1 $argv
 end
 
 
-function fx
+function cxx
+    compile_debug_on_print_off -o a main.cpp
+end
+
+
+function compile_debug_off_print_on
     clang++                                                                                \
                                                                                            \
     -std=c++17 -stdlib=libc++                                                              \
     -DLOCAL -DDEBUG_PRINT                                                                  \
     -O2                                                                                    \
-    -include-pch {$atcoder_dir}/header/precompiled/debug_off_print_on/headers.hpp.pch \
+    -include-pch {$atcoder_dir}/header/precompiled/debug_off_print_on/headers.hpp.pch      \
                                                                                            \
     -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion      \
     -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo          \
-    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                   \
-                                                                                           \
-    -o a main.cpp
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1 $argv
 end
 
 
-function fxx
+function fx
+    compile_debug_off_print_on -o a main.cpp
+end
+
+
+function compile_debug_off_print_off
     clang++                                                                                 \
                                                                                             \
     -std=c++17 -stdlib=libc++                                                               \
     -DLOCAL                                                                                 \
     -O2                                                                                     \
-    -include-pch {$atcoder_dir}/header/precompiled/debug_off_print_off/headers.hpp.pch \
+    -include-pch {$atcoder_dir}/header/precompiled/debug_off_print_off/headers.hpp.pch      \
                                                                                             \
     -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion       \
     -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo           \
-    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                    \
-                                                                                            \
-    -o a main.cpp
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1 $argv
+end
+
+
+function fxx
+    compile_debug_off_print_off -o a main.cpp
+end
+
+
+function tgr
+    rm -rf g
+    mkdir g
+    mkdir g/s
+    mkdir g/c
+    cp main.cpp g/s/main.cpp
+    cp main.cpp g/s/naive.cpp
+    echo "\n\n---\n\n" > g/s/s.txt
+end
+
+
+function tgu
+    cp main.cpp g/s/main.cpp
+end
+
+
+function tg1
+    compile_debug_on_print_off -o g/s/main.out g/s/main.cpp
+    python3 {$atcoder_dir}/testcase-generator/main.py \
+    test ./g/s/main.out                               \
+    -i g/s/s.txt -p g/c/ -c $argv
+end
+
+
+function tg2 
+    compile_debug_on_print_off -o g/s/main.out g/s/main.cpp
+    compile_debug_on_print_off -o g/s/naive.out g/s/naive.cpp
+    python3 {$atcoder_dir}/testcase-generator/main.py \
+    test ./g/s/main.out ./g/s/naive.out               \
+    -i g/s/s.txt -p g/c/ -c $argv
 end
 
