7a354976268912faf3aa78b90576406fe6b6421f
naskya <m@naskya.net>
2022-08-30 04:17:56 +0900

    Update compiling commands

--- commands.fish
+++ commands.fish
@@ -16,3 +16,67 @@ function contest -d "Prepare for atcoder contests"
     nvim .
 end
 
+
+function cx
+    clang++                                                                               \
+                                                                                          \
+    -std=c++17 -stdlib=libc++                                                             \
+    -DLOCAL -DDEBUG -DDEBUG_PRINT                                                         \
+    -D_LIBCPP_DEBUG_LEVEL=2 -O0 -g3 -fsanitize=address,undefined                          \
+    -include-pch ~/workspace/atcoder/header/precompiled/debug_on_print_on/headers.hpp.pch \
+                                                                                          \
+    -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion     \
+    -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo         \
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                  \
+                                                                                          \
+    -o a main.cpp
+end
+
+
+function cxx
+    clang++                                                                                \
+                                                                                           \
+    -std=c++17 -stdlib=libc++                                                              \
+    -DLOCAL -DDEBUG                                                                        \
+    -D_LIBCPP_DEBUG_LEVEL=2 -O0 -g3 -fsanitize=address,undefined                           \
+    -include-pch ~/workspace/atcoder/header/precompiled/debug_on_print_off/headers.hpp.pch \                                                                                
+                                                                                           \
+    -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion      \
+    -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo          \
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                   \
+                                                                                           \
+    -o a main.cpp
+end
+
+
+function fx
+    clang++                                                                                \
+                                                                                           \
+    -std=c++17 -stdlib=libc++                                                              \
+    -DLOCAL -DDEBUG_PRINT                                                                  \
+    -O2                                                                                    \
+    -include-pch ~/workspace/atcoder/header/precompiled/debug_off_print_on/headers.hpp.pch \
+                                                                                           \
+    -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion      \
+    -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo          \
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                   \
+                                                                                           \
+    -o a main.cpp
+end
+
+
+function fxx
+    clang++                                                                                 \
+                                                                                            \
+    -std=c++17 -stdlib=libc++                                                               \
+    -DLOCAL                                                                                 \
+    -O2                                                                                     \
+    -include-pch ~/workspace/atcoder/header/precompiled/debug_off_print_off/headers.hpp.pch \
+                                                                                            \
+    -Wall -Wextra -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion       \
+    -Winit-self -Winvalid-pch -Wmultichar -Wredundant-decls -Wshadow -Wsign-promo           \
+    -Wunused-const-variable -Wno-reorder -ferror-limit=1                                    \
+                                                                                            \
+    -o a main.cpp
+end
+
